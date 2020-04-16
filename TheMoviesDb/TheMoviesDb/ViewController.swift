54687d755a88cb0b2159c8523e22994020587303
AnhNguyen <oliver.anh.nguyen@gmail.com>
2020-04-16 10:43:59 +0700

    [App] create project

---
+++ TheMoviesDb/TheMoviesDb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMoviesDb
+//
+//  Created by admin on 4/16/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
